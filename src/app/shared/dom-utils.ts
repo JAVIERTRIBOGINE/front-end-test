ca7b0617f1ea1c0af0c1c4fd098a992db42f53cf
Serginho <yo@sergiocancelo.es>
2023-09-19 14:48:05 +0200

    impr: improve docs

--- src/app/shared/dom-utils.ts
+++ src/app/shared/dom-utils.ts
@@ -1,34 +1,51 @@
-export function calculateElementWidth(element, includeMargin = false) {
-  let style = element.currentStyle || window.getComputedStyle(element);
+export function calculateElementWidth(
+  element: HTMLElement,
+  includeMargin: boolean = false
+): number {
+  let style = window.getComputedStyle(element);
   let width = element.offsetWidth;
-  let margin = 0, padding = 0;
+  let margin = 0,
+    padding = 0;
   if (includeMargin) {
     margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
-    padding = parseFloat(style.paddingLeft) +
-              parseFloat(style.paddingRight);
+    padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
   }
-  let border = parseFloat(style.borderLeftWidth) +
-               parseFloat(style.borderRightWidth);
+  let border =
+    parseFloat(style.borderLeftWidth) + parseFloat(style.borderRightWidth);
 
   return width + margin - padding + border;
 }
 
-export function calculateElementHeight(element, includeMargin = false) {
-  let style = element.currentStyle || window.getComputedStyle(element);
+export function calculateElementHeight(
+  element: HTMLElement,
+  includeMargin: boolean = false
+): number {
+  let style = window.getComputedStyle(element);
   let height = element.offsetHeight;
-  let margin = 0, padding = 0;
+  let margin = 0,
+    padding = 0;
   if (includeMargin) {
     margin = parseFloat(style.marginLeft) + parseFloat(style.marginRight);
-    padding = parseFloat(style.paddingLeft) +
-              parseFloat(style.paddingRight);
+    padding = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
   }
-  let border = parseFloat(style.borderTopWidth) +
-               parseFloat(style.borderBottomWidth);
+  let border =
+    parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth);
 
   return height + margin - padding + border;
 }
 
-export function calculateScrollPercentage(element): number {
-  let h = element, b = document.body, st = 'scrollTop', sh = 'scrollHeight';
-  return (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+export function calculateScrollPercentage(element: HTMLElement): number {
+  const elementScrollTop = element.scrollTop;
+  const bodyScrollTop = document.body.scrollTop;
+  const elementScrollHeight = element.scrollHeight;
+  const bodyScrollHeight = document.body.scrollHeight;
+  const elementClientHeight = element.clientHeight;
+
+  const scrollTop = elementScrollTop || bodyScrollTop;
+  const scrollHeight = elementScrollHeight || bodyScrollHeight;
+
+  const scrollPercentage =
+    (scrollTop / (scrollHeight - elementClientHeight)) * 100;
+
+  return scrollPercentage;
 }
